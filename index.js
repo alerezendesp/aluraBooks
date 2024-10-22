9c5d33f448d6001484c77e2db2cf8ccf3249635c
GUSTAVO ALEXANDRE SALVADOR REZENDE <0000111458308xsp@aluno.educacao.sp.gov.br>
2024-10-22 08:15:03 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Swiper 
+
+const swiper = new Swiper(".swiper", {
+    spaceBetween: 10,
+    slidesPerView: 3,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+    },
+  });
